6b4e21c3c3e555a1f61541e156c7e644b06b964f
Simon Mai <maismin@gmail.com>
2019-11-11 07:32:49 -0500

    Add security, cors, and morgan

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ dotenv.config({
 });
 
 const express = require('express');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const connectDB = require('./config/database');
 const errorHandler = require('./middleware/error');
@@ -19,6 +26,33 @@ const app = express();
 
 // Boder parser
 app.use(express.json());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+// Prevent HTTP parameter pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(mainRouter);
 
