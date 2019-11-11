69b9de86d7a884b26f142e5cf204140f8deb72ad
Simon Mai <maismin@gmail.com>
2019-11-11 04:47:37 -0500

    Change database.js to log info during development

--- config/database.js
+++ config/database.js
@@ -16,7 +16,12 @@ const connectDB = async () => {
     useFindAndModify: false,
     useUnifiedTopology: true,
   });
-  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold,
+    );
+  }
 };
 
 module.exports = connectDB;
